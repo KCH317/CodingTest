689fcc6303bf1f2d557bc5abbcfd2f1e5515c556
KCH317 <rnjscksgh94@gmail.com>
2021-06-21 23:31:46 +0900

    구현
    왕실의 나이트, 시각

---
+++ 이것이코딩테스트다/구현/시각.py
@@ -0,0 +1,17 @@
+# 시각
+# 정수 N이 입력되면 00시 00분 00초부터 N시 59분 59초까지의 모든 시각 중에서 3이 하나라도 포함되는 모든 경우의 수를 구하는 프로그램을 작성하시오.
+# 입력
+# 5
+# 출력
+# 11475
+
+n = int(input())
+cnt = 0
+
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) + str(m) + str(s):
+                cnt += 1
+
+print(cnt)
